7925eccfd0de1ae1908622c69bde8ddeff7fa101
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2022-12-11 23:01:07 -0500

    Update laundry.php

--- laundry.php
+++ laundry.php
@@ -13,16 +13,24 @@ $Confirm   = $_POST["Confirm"];
 $conn = mysqli_connect("localhost","root","root","laundry");
 if(!$conn)
 	echo "NO CONNECTION";
-else
-{
-	echo "SUCESSFULLY CONNECTED!</br>";
-	$sql="insert into laundrydata (Apartment, Password, Confirm) 
-	values ('$Apartment','$Password','$Confirm')";
-	if(mysqli_query($conn,$sql))
-		echo "ROW INSERTED.";
-	else
-		echo "SORRY, ROW NOT INSERTED BECAUSE THE APARTMENT IS ALREADY INSERTED.";
-}
+else{
+	$query = "select * from laundrydata where Apartment = '$Apartment'";
+	$res= mysqli_query($conn,$query);
+	if(mysqli_num_rows($res)>0){
+		$row = mysqli_fetch_assoc($res);
+		if($Apartment == isset($row['Apartment'])){
+			echo "Apartment is already taken";
+		}
+	}else
+	{
+		echo "SUCESSFULLY CONNECTED!</br>";
+		$sql="insert into laundrydata (Apartment, Password, Confirm) 
+		values ('$Apartment','$Password','$Confirm')";
+		$result = (mysqli_query($conn,$sql));
+		if($result){
+
+			echo "ROW INSERTED.";
+		}
 ?>
 </body>
 </html>
