812bccf38056b85be208f7772dd190d2ae709807
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2022-12-14 21:14:54 -0500

    Update laundry.php

--- laundry.php
+++ laundry.php
@@ -13,21 +13,28 @@ $pass = $_SESSION['Password'];
 ?>
 
 <!DOCTYPE html>
-<link rel = "stylesheet" href = "style.css">
+
 <html>
 <head>   
-<h1> SIGN UP SUCCESSFULLY </h1>
 </head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 <body>
-	
+
 <?php
+
+
+
 $Apartment = $_POST["Apartment"];
 $Password  = $_POST["Password"];
 $Confirm   = $_POST["Confirm"];
-
+	
 $conn = mysqli_connect("localhost","root","root","laundry");
-if(!$conn)
+if(!$conn){
 	echo "NO CONNECTION";
+}
+
+	
 else{
 	$query = "select * from laundrydata where Apartment = '$Apartment'";
 	$res= mysqli_query($conn,$query);
@@ -43,11 +50,17 @@ else{
 		values ('$Apartment','$Password','$Confirm')";
 		$result = (mysqli_query($conn,$sql));
 		if($result){
-			echo "<h5>Thank you for signing up to apartment".$apartment."</h5>";
-			echo "<h2>To go to your account home page click on the link below.</h2>";
-			echo "<button id='account' name='account'><a href='account.php'>Account Home</a></button>";
+			echo "<h1 class='h1'>Thank you for signing up to Apartment ".$apartment."</h1>";
+			echo "<h2 class='h2'>To go to your account home page click on the link below.</h2>";
+			echo "<a class='btn btn-primary btn-large' href='account.php' role='button' >Account Home</a>";
 			//echo "ROW INSERTED.";
 		}
+	}
+}
 ?>
+
+
+
+
 </body>
 </html>
