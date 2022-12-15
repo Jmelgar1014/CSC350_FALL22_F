71d645888a7c0652fb1856a924aa39e1816b7f9e
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2022-12-14 21:15:16 -0500

    Update account.php

--- account.php
+++ account.php
@@ -14,7 +14,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Home</title>
 </head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 <body>
-    <h5>Welcome <?php echo $apartment ?></h5>
+    <h1 class="h1 d-flex justify-content-center">Welcome <?php echo $apartment ?></h1>
 </body>
 </html>
