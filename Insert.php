c5519be10720136521b8d4d52ec4131b02ded50c
nacero01 <99772003+nacero01@users.noreply.github.com>
2022-12-22 00:32:32 -0500

    Update Insert.php

--- Insert.php
+++ Insert.php
@@ -1,26 +1,29 @@
 <?php
 	$servername="localhost";
 	$username="root";
-	$password= "root";
+	$password= "root1234!";
 	$connect=mysqli_connect($servername,$username,$password);
 	//checking connection
 	if(!$connect)  die("Error"); //else echo "connected";
 
-$check="Select *FROM laundry.WeekSchedule WHERE timeinterval='".$_REQUEST['timeSlot']."' AND nameofday='".$_REQUEST['NoD']."'AND Apartment= "" ";
+$check="SELECT * FROM laundry.WeekSchedule WHERE timeinterval='".$_REQUEST['TimeSlot']."' AND nameofday='".$_REQUEST['NoD']."' AND Apartment=''";
 
-$result= mysqli_query($connect, $check);		
-		//echo $sql;
+$result= mysqli_query($connect, $check);
 session_start();
 if(mysqli_num_rows($result)>0)
 {
-$sql = "INSERT INTO laundry.WeekSchedule WHERE timeinterval='".$_REQUEST['timeSlot']."' AND nameofday='".$_REQUEST['NoD']."' (Apartment)
-	VALUES ('".$_SESSION['Apartment']."')";
+	$sql = 
+	"UPDATE laundry.WeekSchedule SET Apartment='".$_SESSION['Apartment']."' WHERE timeinterval='".$_REQUEST['TimeSlot']."' AND nameofday='".$_REQUEST['NoD']."' ";
 
 $result = mysqli_query($connect, $sql); 
 	// Send the query to the database
 	if($result)
 	{
-		echo "You have been Schedule for: ".$_SESSION['timeSlot']." at ".$_SESSION['NoD']." ";
+		echo "<table border='1'><tr><td><center>Date</center></td><td><center>Slot</center></td>";
+		echo "<h2>You have been Schedule for: </h2>";
+		echo "<tr><td><center>".$_REQUEST['NoD']." </center></td>"."<td><center>" .$_REQUEST['TimeSlot']." </td></tr><center>";
+		echo "</table>";
+
 	}
 
 }		
