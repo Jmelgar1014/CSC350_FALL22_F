db8b4468efbfa56a092f66261c0619247d59e81a
nacero01 <99772003+nacero01@users.noreply.github.com>
2022-12-22 00:32:06 -0500

    Update account.php

--- account.php
+++ account.php
@@ -1,44 +1,45 @@
 <?php
-$mysqli = new mysqli( "localhost","root","root", "laundry") or die("Error " . mysqli_error($conn));;
+$mysqli = new mysqli( "localhost","root","root1234!", "laundry") or die("Error " . mysqli_error($conn));;
 if(!$mysqli){
   echo "Failed to connect to Database";
 }
 session_start();
 $sql = "SELECT * FROM WeekSchedule WHERE Apartment='".$_SESSION['Apartment']."'"; 
 
-$result = mysqli_query($connect, $sql); 	// Send the query to the database
+$result = mysqli_query($mysqli, $sql); 	// Send the query to the database
 
+$sql3 = "select * from weekschedule";
+$result3 = mysqli_query($mysqli, $sql3);
 
+{
 	if (mysqli_num_rows($result) > 0) 			// If there are rows present
 	{
-		echo "This is your time slot "; 
-		echo "<table border='1'><tr><td>Date</td><td>Slot</td>";
+		echo "<h3>This is your time slot<h3> "; 
+		echo "<table border='1'><tr><td><center>Date</center></td><td><center>Slot</center></td>";
 		while($row = mysqli_fetch_assoc($result)) 									// fetch next row
 		{ 																			// display the data
-			echo "<tr><td>". $row["nameofday"]."</td><td>". $row["timeinterval"]."</td></tr>";
+			echo "<tr><td><center>". $row["nameofday"]."</center></td><td>". $row["timeinterval"]."</td></tr>";
 			//output data of the row
 		}
 		echo "</table>"; 
 	}
-	else
-	{
-		
-		header('Location:Scheduler.php');
+	else if (mysqli_num_rows($result3) == 0){
+    $mysqli->query("CALL LoadDate()");
+    header('Location: Scheduler.php');
 	}
-
-
-$sql3 = "select * from weekschedule";
-$result3 = mysqli_query($mysqli, $sql3);
-if(mysqli_num_rows($result3) == 0){
-$mysqli->query("CALL LoadDate()");
-header('Location: Scheduler.php');}
-
-$day = date("Y-m-d G:i:s");	
-$sql2 = "SELECT FULLDATE FROM WeekSchedule ORDER BY FULLDATE DESC LIMIT 1";  
-$result2 = mysqli_query($mysqli, $sql2);
-while ($row = mysqli_fetch_assoc($result2)){
-if ($day > $row['FULLDATE'])
-$mysqli->query("CALL reset()");
-header('Location: Scheduler.php');
+	else if(mysqli_num_rows($result3) > 0){
+	header('Location: Scheduler.php');
+	}
+    else
+	{
+    $day = date("Y-m-d G:i:s");	
+    $sql2 = "SELECT FULLDATE FROM WeekSchedule ORDER BY FULLDATE DESC LIMIT 1";  
+    $result2 = mysqli_query($mysqli, $sql2);
+    while ($row = mysqli_fetch_assoc($result2)){
+    if ($day > $row['FULLDATE'])
+    $mysqli->query("CALL reset()");
+    header('Location: Scheduler.php');
+    }
+	}
 }
 ?>
