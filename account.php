8148805f3e93cdf0b1131379267f7663a354bc84
nacero01 <99772003+nacero01@users.noreply.github.com>
2022-12-21 20:01:19 -0500

    Update account.php

--- account.php
+++ account.php
@@ -1,27 +1,4 @@
 <?php
-    session_start();
-
-    $apartment = $_SESSION['Apartment'];
-    $pass = $_SESSION['Password'];
-
-?>
-/*
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Home</title>
-</head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-<body>
-    <h1 class="h1 d-flex justify-content-center">Welcome <?php echo $apartment ?></h1>
-</body>
-</html> */
-  
-<?php
 $mysqli = new mysqli( "localhost","root","root", "laundry") or die("Error " . mysqli_error($conn));;
 if(!$mysqli){
   echo "Failed to connect to Database";
